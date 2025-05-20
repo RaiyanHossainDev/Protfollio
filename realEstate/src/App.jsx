0e75c0a9e4e44164057b49c4318ea596a5d2fa2c
RaiyanHossainDev <astralfog2@gmail.com>
2025-05-20 22:03:31 +0600

    aa

--- realEstate/src/App.jsx
+++ realEstate/src/App.jsx
@@ -2,18 +2,29 @@ import { useState ,React} from 'react'
 import './App.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout/Layout'
-import Home from './Pages/home'
+import Banner from './Components/Banner/Banner'
+import Image from './Components/Image/Image'
+import Service from './Components/Service/Service'
+import Diffrence from './Components/Diffrence/Diffrence'
+import Team from './Components/Team/Team'
+import Story from './Components/Story/Story'
+import News from './Components/News/News'
+import Collection from './Components/Collection/Collection'
+import Footer from './Components/Footer/Footer'
 
 function App() {
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path='/' element={<Layout/>} >
-      <Route index element={<Home/>} />
-    </Route>
-  ))
 
   return (
     <>
-      <RouterProvider router={router} />
+      <Banner/>
+      <Image/>
+      <Service/>
+      <Diffrence/>
+      <Team/>
+      <Story/>
+      <News/>
+      <Collection/>
+      <Footer/>
     </>
   )
 }
